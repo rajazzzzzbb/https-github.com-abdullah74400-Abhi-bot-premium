d817bbc3a21360e3b7ced6e280714b5fe1b7ef7f
Abhishek Kumar <Nain95552@gmail.com>
2025-09-12 16:35:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ CODE = (environ.get('CODE', 'https://t.me/Movie_house_club'))
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'arlinks.in'))
 STREAM_API = (environ.get('STREAM_API', 'c8deaa7cb811330e142c77d4050754c9cda60dfa'))
-STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/Hindi_movie_uplod/36'))
+STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/Movie_house_club'))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1807927334 7436687322').split()]
